757e0197cddc5740753e97683f2298811b8a019f
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-07-14 17:38:20 -0500

    time blocks added, tasks can be dynamically updated and saved to page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,46 @@
-var Today = (moment().format("MMMM D, YYYY"));
-    console.log(Today)
-
-var currentDate = function(){
+var Today = (moment().format("MMMM D, YYYY"))
     $("#currentDay").text(Today);
-}
 
-currentDate();
+//Task update with click
+$("#task").on("click", "p", function(){
+    console.log("<p> was clicked");
+    var text =$(this)
+      .text()
+      .trim();
+    var textInput =$("<textarea>")
+      .addClass("form-control")
+      .val(text);
+  
+    $(this).replaceWith(textInput);
+     textInput.trigger("focus");
+  });
+
+  //Task needs to be updated
+$("#task").on("blur", "textarea", function(){
+  //get the textareas; current value/text
+    var text = $(this)
+      .val()
+      .trim();
+    console.log(text)
+//     // get teh parent ul's id attribute
+//     var status = $(this)
+//       .closest(".list-group")
+//       .attr("id")
+//       .replace("list-", "");
+
+//     //get the task's position in the list of other li elements
+//     var index = $(this)
+//       .closest(".list-group-item")
+//       .index();
+
+//     tasks[status][index].text = text;
+//     saveTasks();
+
+    //recreate p element
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(text);
+
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
+  });    
