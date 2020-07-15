b237f731b722d69dd04571ea0b9ed7c75b5a9bcb
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-07-15 13:58:22 -0500

    color coding based on current hour

--- assets/script.js
+++ assets/script.js
@@ -8,24 +8,38 @@ var loadTasks = function(){
     }
     for (i=0; i<tasks.length; i++){
         var index = i
-        console.log(index);
-        console.log(tasks[i])
+        // console.log(index);
+        // console.log(tasks[i])
         
         var firstHour = index + 8
         var taskP = $("<p>").addClass("description task-item-" + firstHour).text(tasks[i])
-        console.log(firstHour)
-        console.log(taskP);
-        $(".task-" + firstHour).append(taskP);
+        
+        // console.log(firstHour)
+        // console.log(taskP);
+        $("#task-" + firstHour).append(taskP);
     }
 }
     
 var Today = (moment().format("MMMM D, YYYY"))
     $("#currentDay").text(Today);
 
+//color code hours bins
+    var currentHour = moment().hour() 
+     
+    for(var i=8; i<17; i++){
+        var taskArea = $("#task-"+i)  
+        if(currentHour<i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        }else{
+            $(taskArea).addClass("future")
+        }
+    }
 
 //Task update with click
 $(".taskBin").on("click", "p", function(){
-    console.log("<p> was clicked");
+    // console.log("<p> was clicked");
     var text =$(this)
       .text()
       .trim();
@@ -54,12 +68,11 @@ $(".taskBin").on("blur", "textarea", function() {
     $(this).replaceWith(taskP);
   });    
 
-  //save tasks
-
+  //Save tasks
   $(".saveBtn").on("click", function(){
     //   console.log("<save button> was clicked");
       var index = $(".saveBtn").index(this);
-      console.log(index)
+    //   console.log(index)
       tasks[index] = $(this).parent().find("p").text();
     //   console.log(tasks)
       localStorage.setItem("tasks", JSON.stringify(tasks));
